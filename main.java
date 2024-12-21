e9ce13d24ee83f42062aa2202baa14bafc5c5f50
Francis Lean <167146805+Francis-Lean@users.noreply.github.com>
2024-12-20 22:00:02 -0300

    Add files via upload
    Testando entrada de dados em JAVA

---
+++ main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main (String[]args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X;
+        X = sc.nextInt();
+        System.out.printf("Você digitou: %s", X);
+
+
+
+
+        sc.close();
+    }
+
+}
